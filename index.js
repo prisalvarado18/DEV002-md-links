89eb1e7f063f2f112f3942fb96875e90914c25cf
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2023-01-26 09:28:35 -0500

    Imported the File System Module

--- index.js
+++ index.js
@@ -4,3 +4,6 @@
 
 // Access to Node core modules
 const path = require('path');
+// Import the Node.js file system module to work with the file system
+// It reads, creates, updates, deletes, and renames files
+const fs = require('fs');
