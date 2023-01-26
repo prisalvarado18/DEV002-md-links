414fc2b7ebd005e89537390448cdf040d06e8620
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2023-01-26 13:46:18 -0500

    Added tests for isDirectory

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -8,11 +8,12 @@
 //   });
 
 // });
-const { fileExists, checkPath, convertToAbsolutePath } = require('../index.js');
+const { fileExists, checkPath, convertToAbsolutePath, isDirectory } = require('../index.js');
 
 const realAbsolutePath = 'C:/Users/palva/OneDrive/Documents/proyectosLaboratoria/DEV002-md-links/test/folder/archive.md';
 const falseRelativePath = './notRealfolder/archive.md';
 const realRelativePath = 'test/folder/archive.md'
+const dirPath = 'C:/Users/palva/OneDrive/Documents/proyectosLaboratoria/DEV002-md-links/test/folder';
 
 describe('fileExists', () => {
   it('fileExists should be a function', () => {
@@ -44,5 +45,17 @@ describe('convertToAbsolutePath', () => {
   });
   it('should convert the path entered to an absolute one', () => {
     expect(convertToAbsolutePath(realRelativePath)).toEqual('C:\\Users\\palva\\OneDrive\\Documents\\proyectosLaboratoria\\DEV002-md-links\\test\\folder\\archive.md');
+  });
+});
+
+describe('isDirectory', () => {
+  it('should be a function', () => {
+    expect(typeof isDirectory).toBe('function');
+  });
+  it('should return true if the given path points to a Directory', () => {
+    expect(isDirectory(dirPath)).toEqual(true);
+  });
+  it('should return false if the given path does not point to a Directory', () => {
+    expect(isDirectory(realAbsolutePath)).toEqual(false);
   });
 });
