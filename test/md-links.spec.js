8545f8b76403f679267ab682574db9cb4074e55c
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2023-01-26 13:27:27 -0500

    Added some changes to grammar errors

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -18,10 +18,10 @@ describe('fileExists', () => {
   it('fileExists should be a function', () => {
     expect(typeof fileExists).toBe('function');
   });
-  it('It should return true if the path exists', () => {
+  it('should return true if the path exists', () => {
     expect(fileExists(realAbsolutePath)).toEqual(true);
   });
-  it('It should return true if the path does not exist', () => {
+  it('should return true if the path does not exist', () => {
     expect(fileExists(falseRelativePath)).toEqual(false);
   });
 });
@@ -30,10 +30,10 @@ describe('checkPath', () => {
   it('checkPath should be a function', () => {
     expect(typeof checkPath).toBe('function');
   });
-  it('It should return true if the is absolute', () => {
+  it('should return true if the is absolute', () => {
     expect(checkPath(realAbsolutePath)).toEqual(true);
   });
-  it('It should return false if the path is not absolute', () => {
+  it('should return false if the path is not absolute', () => {
     expect(checkPath(realRelativePath)).toEqual(false);
   });
 });
@@ -42,7 +42,7 @@ describe('convertToAbsolutePath', () => {
   it('convertToAbsolutePath should be a function', () => {
     expect(typeof convertToAbsolutePath).toBe('function');
   });
-  it('It should convert the path entered to an absolute one', () => {
+  it('should convert the path entered to an absolute one', () => {
     expect(convertToAbsolutePath(realRelativePath)).toEqual('C:\\Users\\palva\\OneDrive\\Documents\\proyectosLaboratoria\\DEV002-md-links\\test\\folder\\archive.md');
   });
 });
