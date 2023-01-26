d4631dd29ee48b114f22af559b6778aa7b100638
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2023-01-26 13:26:37 -0500

    Added tests for convertToAbsolutePath

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -8,32 +8,41 @@
 //   });
 
 // });
-const { fileExists, checkPath } = require('../index.js');
+const { fileExists, checkPath, convertToAbsolutePath } = require('../index.js');
 
 const realAbsolutePath = 'C:/Users/palva/OneDrive/Documents/proyectosLaboratoria/DEV002-md-links/test/folder/archive.md';
 const falseRelativePath = './notRealfolder/archive.md';
 const realRelativePath = 'test/folder/archive.md'
 
 describe('fileExists', () => {
-    it('fileExists should be a function', () => {
-        expect(typeof fileExists).toBe('function');
-    });
-    it('It should return true if the path exists', () => {
-        expect(fileExists(realAbsolutePath)).toEqual(true);
-      });
-      it('It should return true if the path does not exist', () => {
-        expect(fileExists(falseRelativePath)).toEqual(false);
-      });
+  it('fileExists should be a function', () => {
+    expect(typeof fileExists).toBe('function');
+  });
+  it('It should return true if the path exists', () => {
+    expect(fileExists(realAbsolutePath)).toEqual(true);
+  });
+  it('It should return true if the path does not exist', () => {
+    expect(fileExists(falseRelativePath)).toEqual(false);
+  });
 });
 
 describe('checkPath', () => {
-    it('checkPath should be a function', () => {
-        expect(typeof checkPath).toBe('function');
-    });
-    it('It should return true if the is absolute', () => {
-        expect(checkPath(realAbsolutePath)).toEqual(true);
-      });
-      it('It should return false if the path is not absolute', () => {
-        expect(checkPath(realRelativePath)).toEqual(false);
-      });
+  it('checkPath should be a function', () => {
+    expect(typeof checkPath).toBe('function');
+  });
+  it('It should return true if the is absolute', () => {
+    expect(checkPath(realAbsolutePath)).toEqual(true);
+  });
+  it('It should return false if the path is not absolute', () => {
+    expect(checkPath(realRelativePath)).toEqual(false);
+  });
+});
+
+describe('convertToAbsolutePath', () => {
+  it('convertToAbsolutePath should be a function', () => {
+    expect(typeof convertToAbsolutePath).toBe('function');
+  });
+  it('It should convert the path entered to an absolute one', () => {
+    expect(convertToAbsolutePath(realRelativePath)).toEqual('C:\\Users\\palva\\OneDrive\\Documents\\proyectosLaboratoria\\DEV002-md-links\\test\\folder\\archive.md');
+  });
 });
