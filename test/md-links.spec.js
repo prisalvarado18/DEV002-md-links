ee619435a212902102110c8e207b5723ea35abff
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2023-01-26 12:54:39 -0500

    Added tests to checkPath

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -8,19 +8,32 @@
 //   });
 
 // });
-const { fileExists } = require('../index.js');
+const { fileExists, checkPath } = require('../index.js');
 
-const realPath = 'C:/Users/palva/OneDrive/Documents/proyectosLaboratoria/DEV002-md-links/test/folder/archive.md';
-const falsePath = './notRealfolder/archive.md'
+const realAbsolutePath = 'C:/Users/palva/OneDrive/Documents/proyectosLaboratoria/DEV002-md-links/test/folder/archive.md';
+const falseRelativePath = './notRealfolder/archive.md';
+const realRelativePath = 'test/folder/archive.md'
 
 describe('fileExists', () => {
     it('fileExists should be a function', () => {
         expect(typeof fileExists).toBe('function');
     });
     it('It should return true if the path exists', () => {
-        expect(fileExists(realPath)).toEqual(true);
+        expect(fileExists(realAbsolutePath)).toEqual(true);
       });
       it('It should return true if the path does not exist', () => {
-        expect(fileExists(falsePath)).toEqual(false);
+        expect(fileExists(falseRelativePath)).toEqual(false);
+      });
+});
+
+describe('checkPath', () => {
+    it('checkPath should be a function', () => {
+        expect(typeof checkPath).toBe('function');
+    });
+    it('It should return true if the is absolute', () => {
+        expect(checkPath(realAbsolutePath)).toEqual(true);
+      });
+      it('It should return false if the path is not absolute', () => {
+        expect(checkPath(realRelativePath)).toEqual(false);
       });
 });
