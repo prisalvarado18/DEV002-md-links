7febe02679ea713ea82ca7a7f747cf209f20a4ff
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2023-01-26 09:33:01 -0500

    Checked the existence of a path

--- index.js
+++ index.js
@@ -4,3 +4,9 @@
 
 // Access to Node core modules
 const path = require('path');
+
+// Check if path exists
+const fileExists = (pathname) => {
+    const fileExists = fs.existsSync(pathname);
+    return fileExists ? true : false;
+}
