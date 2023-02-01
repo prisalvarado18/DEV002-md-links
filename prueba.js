702bcfd7f9d87c61a244b47f4fe2ce00c8f99a1c
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2023-02-01 16:39:25 -0500

    Added the module functionality

--- prueba.js
+++ prueba.js
@@ -1,4 +1,23 @@
-const { mdLinks } = require('./promise');
+// const { mdLinks } = require('./promise');
 
-mdLinks('./test/folder', '--validate  ""'  )
-    .then(link => console.log(link))
+// mdLinks('./test/folder', '--validate  ""'  )
+//     .then(link => console.log(link))
+
+// mdLinks('./test/folder', ['--validate'])
+//     .then(outcome => {
+//         console.log(outcome);
+//     })
+
+// mdLinks('./test/folder', ['--validate'])
+//     .then(outcome => {
+//         console.log(outcome);
+//     })
+
+
+
+
+/*const { md_Links } = require("./module");
+
+md_Links('./test/folder')
+md_Links('./test/folder', '{ validate: true }')
+md_Links('./test/folder', '{ validate: false }')*/
